25f0339438c724ce4fa8a19700d806e5431d3eb5
Letícia de Abreu <abreuhleticia@gmail.com>
2025-07-09 08:56:24 -0300

    Foi modificado

--- Produto.java
+++ Produto.java
@@ -1,8 +1,31 @@
 public class  Produto{
     // //Atributos (caracteristicas e campos/variaveis)
-    String nome;
-    double preco;
-    int quantidade;
+    //public outras classes podem ter acesso a essa classe
+    public String nome;
+    public double preco;
+    public int quantidade;
+
+    //Metodos( ação e compotamento)
+      
+            //Metodos
+            public double mensagem();
+            String mensagem = "Peoduto: "+nome+", R$ "+preco+", "+quantidade+" unidade, Total: "+(preco*quantidade);
+
+            //retorna valor em estoque
+            public double TotalEmEstoque(){
+                return preco * quantidade;
+            }
+
+            //void pois não tem retorno para função principal
+            //envio a quantidade de numero q quero encrementar e ele adiciona ao anterior
+            public void AdicionarProduto(int qtde){
+            quantidade += qtde;
+            }
+
+            //ira remover um quatidade do estoque
+            public void RemoveProduto(int qtde){
+                quantidade -= qtde;
+            }
 
     
 
--- Programa.java
+++ Programa.java
@@ -5,18 +5,35 @@ public class Programa{
     public static void main(String[] args){
          Locale.setDefault(new Locale("pt", "BR"));
          Scanner scanner = new Scanner(System.in);
+        
+        //Criando o produto
+         Produto produto1 = new Produto();
 
-            
+        //Cadastro o produto 1
+         System.out.println("Entre com os dados do Produto:");
+         System.out.println("Nome: ");
+         produto1.nome = scanner.nextLine();
+         System.out.println("Preco");
+         produto.preco= scanner.nextDouble();
+         System.out.println("Quantidade: ");
+         produto1.quantidade=scanner.nextInt();
 
-            Produto produto1 = new Produto();
+        //Mostrar mensagem
+        System.out.println(produto1.mensagem());
 
-            produto1.nome='Tv';
-            produto1.preco= 100;
-            produto1.quantidade=30;
+        //Adicionar estoque
+        System.out.println("Entre com o numero de produtos para adicionar no estoque: ");
+        int estoque = scanner.nextInt();
+        produto1.AdicionarProduto(estoque);
 
-            System.out.println("Nome do produto: ");
-            System.out.prinln("Preço do produto: ");
-            System.out.println("Quantidade do produto em estoque: ");
+        //Mostrar mensagem
+        System.out.printf("Eentre com numero de produtos para remover do estoque: ");
+        estoque = scanner.nextInt();
+        produto1.RemoverProduto(estoque);
+
+        //Mostrar mensagem
+        System.out.println(produto1.mensagem());
+
 
     }
 }
